98446202446c9ae8ed95cd452986973bc6723ba3
rishabhvyas17 <rishabhvyas222@gmail.com>
2026-03-26 14:38:17 +0530

    mouse events

--- events.js
+++ events.js
@@ -1,8 +1,32 @@
 let btn = document.getElementById("btn");
 let msg = document.getElementById("msg");
 
-btn.addEventListener("click",show);
-
-function show(){
+btn.addEventListener("click",click);
+function click(){
     msg.innerHTML = "I am clicked";
 }
+
+//mouse events
+
+btn.addEventListener("dbclick", function(event){
+    msg.innerHTML ="I am double clicked";
+});
+btn.addEventListener("mouseover",function(event){
+    msg.innerHTML="Somebody has put mouse on me!!!";
+});
+btn.addEventListener("mouseout",function(event){
+    msg.innerHTML="Thank God! I am free now!!";
+    console.log(event);
+    console.log(event.type);
+    console.log(event.target);
+});
+btn.addEventListener("mousemove",function(event){
+    msg.innerHTML="the mouse just moved";
+});
+
+
+
+
+
+
+
