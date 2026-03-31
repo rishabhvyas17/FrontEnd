30c666d3bb86239fdba167b3da3011bff0c04a10
rishabhvyas17 <rishabhvyas222@gmail.com>
2026-03-31 14:06:00 +0530

    handling promises

--- promises.js
+++ promises.js
@@ -7,7 +7,7 @@ let myPromise = new Promise((resolve,reject)=>{
     else{
         reject("FAILURE");
     }
-})
+});
 
 
 myPromise.then(result =>{
@@ -19,9 +19,30 @@ myPromise.then(result =>{
 .catch(error=>{
     console.log("error",error)
 })
-.finalyy(()=>{
+.finally(()=>{
     console.log("Completed")
+});
+
+
+myPromise.then(result =>{
+    console.log("result",result);
 })
+.catch(error=>{
+    console.log("error".error);
+})
+.finally(()=>{
+    console.log("Completed");
+});
+
+fetch("https://dummyjson.com/products")
+.then(response => response.json())
+.then(data => {
+    console.log(data.products);
+}).catch(error=> {
+    console.log("Error",error);
+});
+
+
 
 //E.g. - Promise is used for dependent tasks , when we want the later one to execute only after the first one is executed completely. It is an example of Atomicity.
 // E.g. - when we use multiple apis so when first api call is success then only next one should be called.
